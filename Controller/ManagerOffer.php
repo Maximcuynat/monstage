06a0a98e958e3fff6e7b500f81cfacef4fa7de81
Labooo <lea.laborde@viacesi.fr>
2022-03-16 17:15:41 +0100

    Ajout de l data

--- Controller/ManagerOffer.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once '../Model/Offer.php';
-include_once 'Manager.php';
-class ManagerOffer extends Manager
-{
-    
-}
--- View/ViewAccueilStudent.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Acceuille de la page des Etudiant
-echo "Student Page";
-//
-?>
