b9cd24a4af801a97195958bfd06f57b16ad001f5
Labooo <lea.laborde@viacesi.fr>
2022-03-18 10:15:25 +0100

    head + footer

---
+++ View/header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>PROJECT</title>
+  </head>
+
+  <body>
+      <!--========================================================================================= HEADER -->
+      <header>
+          <div>
+            <img class="head_logo" src="SRC/IMG/logo_site_rond.png">
+          </div>
+          <div><h1 class="head_titre">My CESI Internship</h1></div>
+          <div class="head_profil">
+            <h2 class="NP">&nbsp;Prenom NOM&nbsp;</h2>
+            <img src="SRC/IMG/user.png">
+            <img href="#" class="btn_menu" src="SRC/IMG/burger_menu_blanc.png">
+          </div>
+      </header>
+  </body>
+</html>
