dbfe5f9ba5f0b318ed30805d3108df1e070f701d
Labooo <lea.laborde@viacesi.fr>
2022-03-21 10:08:27 +0100

    alt = Logo

--- View/header.php
+++ View/header.php
@@ -14,9 +14,11 @@
       <!--========================================================================================= HEADER -->
       <header>
           <div>
-            <img class="head_logo" src="../assets/img/logo_site_rond.png">
+            <img alt="Logo" class="head_logo" src="../assets/img/logo_site_rond.png">
           </div>
-          <div><h1 class="head_titre">My CESI Internship</h1></div>
+          <div>
+            <h1 class="head_titre">My CESI Internship</h1>
+          </div>
           <div class="head_profil">
             <h2 class="NP">&nbsp;Prenom NOM&nbsp;</h2>
             <img src="../assets/img/user.png">
