89a8353c93c50da9d497066701639ead3e1669d3
Merveille Jora <88484917+MerveilleJora@users.noreply.github.com>
2022-03-22 10:43:34 +0100

    add les views

---
+++ View/Admin/viewstatscomanies.php
@@ -0,0 +1,92 @@
+<!DOCTYPE php>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../assets/CSS/stats.css">
+    <link rel="stylesheet" href="../../assets/CSS/generalStyle.css">
+    <link rel="stylesheet" href="../../assets/CSS/footerheader.css">
+    <link rel="stylesheet" href="../../assets/CSS/burger_nav.css">
+    <link rel="icon" class="head_logo" href="../assets/IMG/logo_site_rond.png"/>
+    <title>Statistics</title>
+  </head>
+
+<body>
+<?php
+require "headers/header_admin.php";?>
+<main>
+    <section class="sectionstats">
+    <!-- ================================= Petits Cadre -->
+        <div class="smallformsection">
+            <h1 class="titlestat">The best offers</h1>
+            <div class="Form">
+                <div class="div_left_right">
+                    <div>
+                        <h2>Job</h2>
+                        <label>Company</label><br>
+                        <label>Location</label>
+                    </div>
+                    <div>
+                        <label>Date</label>
+                    </div>
+                </div>
+            </div>
+            <div class="Form">
+                <div class="div_left_right">
+                    <div>
+                        <h2>Job</h2>
+                        <label>Company</label><br>
+                        <label>Location</label>
+                    </div>
+                    <div>
+                        <label>Date</label>
+                    </div>
+                </div>
+            </div>
+            <div class="Form">
+                <div class="div_left_right">
+                    <div>
+                    <h2>Job</h2>
+                        <label>Company</label><br>
+                        <label>Location</label>
+                    </div>
+                    <div>
+                        <label>Date</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+        <div class="smallform">
+            <h1 class="titlestat">The best companies</h1>
+            <div class="Form">
+                <div class="div_left_right">
+                    <div>
+                        <h2>Company</h2>
+                        <label>Location</label>
+                    </div>
+                </div>
+            </div>
+            <div class="Form">
+                <div class="div_left_right">
+                    <div>
+                        <h2>Comany</h2>
+                        <label>Company</label><br>
+                    </div>
+                </div>
+            </div>
+            <div class="Form">
+                <div class="div_left_right">
+                    <div>
+                        <h2>Comany</h2>  
+                        <label>Location</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<?php
+require "footer.php";
+?>
